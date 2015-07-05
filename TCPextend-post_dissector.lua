93e61a7462208b34976bb6929b7c40d00cab6202
Christopher Gadd <gaddman@email.com>
2015-07-05 17:14:09 +1200

    Adding init function to fix #2
    Also changing variable names for readability.

--- TCPextend-post_dissector.lua
+++ TCPextend-post_dissector.lua
@@ -13,73 +13,76 @@
 --
 -- Chris Gadd
 -- https://github.com/gaddman/wireshark-tcpextend
--- v0.5-20150618
+-- v0.6-20150705
 --
 -- Known limitiations:
 -- Any TCP errors (eg retransmissions, OOO) are not correctly handled
 
 -- declare some Fields to be read
-local tcp_len_f = Field.new("tcp.len")
-local tcp_ack_f = Field.new("tcp.ack")
-local tcp_len_f = Field.new("tcp.len")
-local tcp_push_f = Field.new("tcp.flags.push")
-local tcp_seq_f = Field.new("tcp.seq")
-local tcp_stream_f = Field.new("tcp.stream")
-local tcp_win_f = Field.new("tcp.window_size")
-local tcp_src_f = Field.new("tcp.srcport")
-local tcp_dst_f = Field.new("tcp.dstport")
-local tcp_ack_frm_f = Field.new("tcp.analysis.acks_frame")
+local f_tcp_len = Field.new("tcp.len")
+local f_tcp_ack = Field.new("tcp.ack")
+local f_tcp_push = Field.new("tcp.flags.push")
+local f_tcp_seq = Field.new("tcp.seq")
+local f_tcp_stream = Field.new("tcp.stream")
+local f_tcp_win = Field.new("tcp.window_size")
+local f_tcp_src = Field.new("tcp.srcport")
+local f_tcp_dst = Field.new("tcp.dstport")
+local f_tcp_ack_frm = Field.new("tcp.analysis.acks_frame")
 
 -- declare (pseudo) protocol
-local TCPextend_proto = Proto("TCPextend","Extended TCP information")
+local p_TCPextend = Proto("TCPextend","Extended TCP information")
 -- create the fields for this "protocol". These probably shouldn't all be 32 bit integers.
-local bif_F = ProtoField.int32("TCPextend.bif","Bytes in Flight")
-local max_tx_F = ProtoField.int32("TCPextend.max_tx","Max tx bytes")
-local bsp_F = ProtoField.int32("TCPextend.bsp","Bytes since PSH")
-local pba_F = ProtoField.int32("TCPextend.pba","Packets before ACK")
-local delta_F = ProtoField.relative_time("TCPextend.delta","Time delta")
+local F_bif = ProtoField.int32("TCPextend.bif","Bytes in Flight")
+local F_max_tx = ProtoField.int32("TCPextend.max_tx","Max tx bytes")
+local F_bsp = ProtoField.int32("TCPextend.bsp","Bytes since PSH")
+local F_pba = ProtoField.int32("TCPextend.pba","Packets before ACK")
+local F_delta = ProtoField.relative_time("TCPextend.delta","Time delta")
 -- add the fields to the protocol
-TCPextend_proto.fields = {bif_F, max_tx_F, bsp_F, pba_F, delta_F}
--- register protocol as a postdissector
-register_postdissector(TCPextend_proto)
+p_TCPextend.fields = {F_bif, F_max_tx, F_bsp, F_pba, F_delta}
 
--- variables to persist across all packets
-local tcpextend_stats = {}
--- per stream
-tcpextend_stats.client_time = {}
-tcpextend_stats.server_time = {}
-tcpextend_stats.client_port = {}
-tcpextend_stats.server_port = {}
-tcpextend_stats.client_win = {}
-tcpextend_stats.server_win = {}
-tcpextend_stats.client_bsp = {}
-tcpextend_stats.server_bsp = {}
-tcpextend_stats.client_ack = {}
-tcpextend_stats.server_ack = {}
-tcpextend_stats.client_len = {}
-tcpextend_stats.server_len = {}
--- per packet
-tcpextend_stats.bif = {}
-tcpextend_stats.bsp = {}
-tcpextend_stats.txb = {}
-tcpextend_stats.pba = {}
-tcpextend_stats.delta = {}
+local function reset_stats()
+	-- variables to persist across all packets
+	tcpextend_stats = {}
+	-- per stream
+	tcpextend_stats.client_time = {}
+	tcpextend_stats.server_time = {}
+	tcpextend_stats.client_port = {}
+	tcpextend_stats.server_port = {}
+	tcpextend_stats.client_win = {}
+	tcpextend_stats.server_win = {}
+	tcpextend_stats.client_bsp = {}
+	tcpextend_stats.server_bsp = {}
+	tcpextend_stats.client_ack = {}
+	tcpextend_stats.server_ack = {}
+	tcpextend_stats.client_len = {}
+	tcpextend_stats.server_len = {}
+	-- per packet
+	tcpextend_stats.bif = {}
+	tcpextend_stats.bsp = {}
+	tcpextend_stats.txb = {}
+	tcpextend_stats.pba = {}
+	tcpextend_stats.delta = {}
+end
+
+function p_TCPextend.init()
+	reset_stats()
+end
    
 -- function to "postdissect" each frame
-function TCPextend_proto.dissector(extend,pinfo,tree)
+function p_TCPextend.dissector(extend,pinfo,tree)
 
-	local tcp_len = tcp_len_f()
+	local tcp_len = f_tcp_len()
 	if tcp_len then    -- seems like it should filter out TCP traffic. Maybe there's a way like taps to register the dissector with a filter?
 		tcp_len = tcp_len.value
 		local pkt_no = tostring(pinfo.number) -- warning, this will become a large array (of 32bit integers) if lots of packets
 		local frame_time = pinfo.rel_ts
-		local tcp_stream = tcp_stream_f().value
-		local tcp_ack = tcp_ack_f().value
-		local tcp_push = tcp_push_f().value
-		local tcp_win = tcp_win_f().value
-		local tcp_seq = tcp_seq_f().value
-		local tcp_srcport = tcp_src_f().value
-		local tcp_dstport = tcp_dst_f().value
+		local tcp_stream = f_tcp_stream().value
+		local tcp_ack = f_tcp_ack().value
+		local tcp_push = f_tcp_push().value
+		local tcp_win = f_tcp_win().value
+		local tcp_seq = f_tcp_seq().value
+		local tcp_srcport = f_tcp_src().value
+		local tcp_dstport = f_tcp_dst().value
 
 		if not pinfo.visited then
 	
@@ -174,19 +177,22 @@ function TCPextend_proto.dissector(extend,pinfo,tree)
 			end
 			
 			if tcp_ack then
-				tcpextend_stats.pba[pkt_no] = pkt_no - tcp_ack_frm_f().value
+				tcpextend_stats.pba[pkt_no] = pkt_no - f_tcp_ack_frm().value
 			end
 
 		end	-- if packet not visited
 
 		-- packet processed, output to tree
-		local subtree = tree:add(TCPextend_proto,"TCP extended info")
-		subtree:add(delta_F,tcpextend_stats.delta[pkt_no]):set_generated()
-		subtree:add(bsp_F,tcpextend_stats.bsp[pkt_no]):set_generated()
-		subtree:add(bif_F,tcpextend_stats.bif[pkt_no]):set_generated()
-		subtree:add(max_tx_F,tcpextend_stats.txb[pkt_no]):set_generated()
+		local subtree = tree:add(p_TCPextend,"TCP extended info")
+		subtree:add(F_delta,tcpextend_stats.delta[pkt_no]):set_generated()
+		subtree:add(F_bsp,tcpextend_stats.bsp[pkt_no]):set_generated()
+		subtree:add(F_bif,tcpextend_stats.bif[pkt_no]):set_generated()
+		subtree:add(F_max_tx,tcpextend_stats.txb[pkt_no]):set_generated()
 		if tcpextend_stats.pba[pkt_no] then
-			subtree:add(pba_F,tcpextend_stats.pba[pkt_no]):set_generated()
+			subtree:add(F_pba,tcpextend_stats.pba[pkt_no]):set_generated()
 		end
 	end	-- if a TCP packet
 end
+
+-- register protocol as a postdissector
+register_postdissector(p_TCPextend)
